c09ccc10b99da04f5fc9d81af2c4cc21368dc444
Volk <Volked18@gmail.com>
2019-04-11 20:42:12 +0300

    add cells prefab, add outline , add gamecontroller

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,33 @@
+﻿using cakeslice;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    Outline _outline;
+    public float CellSquereSize = 1;
+    private void Awake()
+    {
+        _outline = GetComponent<Outline>();
+        if(_outline == null)
+        {
+            Debug.LogError("Outline class is missing!");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameController.Instance.SetBorder += SetOutline;
+    }
+
+    void SetOutline(int colorIndex)
+    {
+        if(colorIndex > 2 || colorIndex < 0)
+        {
+            Debug.LogError("color invex out of range!");
+            return;
+        }
+        _outline.color = colorIndex;
+    }
+
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public System.Action<int> SetBorder;
+
+    #region Singelton
+
+    private static GameController _instance;
+    public static GameController Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new GameObject("GameController").AddComponent<GameController>();
+            }
+            return _instance;
+        }
+        private set
+        {
+            _instance = value;
+        }
+    }
+
+    #endregion
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            SetBorder?.Invoke(0);
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            SetBorder?.Invoke(2);
+        }
+    }
+}
